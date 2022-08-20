55a4576def0ada6a2a5221851f27c8157908886f
monofy01 <bri_riva@hotmail.com>
2022-08-20 16:12:24 -0500

    [INIT] init project

---
+++ src/main/java/com/brian/vinne/VinneApplication.java
@@ -0,0 +1,13 @@
+package com.brian.vinne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brian/vinne/VinneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.vinne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
